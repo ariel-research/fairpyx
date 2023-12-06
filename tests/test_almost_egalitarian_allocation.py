b9fff6fdf510d78a32b15cbff8a9d277ad1f9678
Erel Segal-Halevi <erelsgl@gmail.com>
2023-12-06 18:18:25 +0200

    tests

---
+++ tests/test_almost_egalitarian_allocation.py
@@ -0,0 +1,33 @@
+"""
+Test the almost-egalitarian algorithm.
+
+Programmer: Erel Segal-Halevi
+Since:  2023-07
+"""
+
+import pytest
+
+import fairpyx
+import numpy as np
+
+NUM_OF_RANDOM_INSTANCES=10
+
+def test_feasibility():
+    for i in range(NUM_OF_RANDOM_INSTANCES):
+        np.random.seed(i)
+        instance = fairpyx.Instance.random_uniform(
+            num_of_agents=21, num_of_items=3, normalized_sum_of_values=1000,
+            agent_capacity_bounds=[2,6], 
+            item_capacity_bounds=[20,40], 
+            item_base_value_bounds=[1,1000],
+            item_subjective_ratio_bounds=[0.5, 1.5]
+            )
+        allocation = fairpyx.divide(fairpyx.algorithms.almost_egalitarian_without_donation, instance=instance)
+        fairpyx.validate_allocation(instance, allocation, title=f"Seed {i}, without donation")
+        allocation = fairpyx.divide(fairpyx.algorithms.almost_egalitarian_with_donation, instance=instance)
+        fairpyx.validate_allocation(instance, allocation, title=f"Seed {i}, with donation")
+
+
+if __name__ == "__main__":
+     pytest.main(["-v",__file__])
+
