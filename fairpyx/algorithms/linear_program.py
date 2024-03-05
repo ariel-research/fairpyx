803af3713a97f525f9f30dfc9ade80d9f93f3bc0
Renana Turgeman <renanaturgeman058@gmail.com>
2024-03-05 11:51:46 +0200

    working on linear_program.py

--- fairpyx/algorithms/linear_program.py
+++ fairpyx/algorithms/linear_program.py
@@ -5,6 +5,7 @@ import logging
 from fairpyx import Instance
 
 
+# TODO - delete this enum from here
 class EFTBStatus(Enum):
     NO_EF_TB = 0
     EF_TB = 1
@@ -16,17 +17,20 @@ logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='%(message)s',
 logger = logging.getLogger()
 
 
-def check_envy(instance, student, idx_student, other_student, idx_other, a):
+# a = {'Alice': {3.5: ('x', 'y'), 3: ('x', 'z')}, 'Bob': {3.5: ('x', 'y'), 2: ('y', 'z')}}
+# a = {'Alice': {3.5: (1, 1, 0), 3: (1, 0, 1)}
+# initial_budgets = {"Alice": 5, "Bob": 4}
+def check_envy(instance, student, other_student, a):
     result = []
     # check if student envies in other_student
-    for idx_i, bundle_i in enumerate(a[idx_student]):
-        for idx_j, bundle_j in enumerate(a[idx_other]):
+    for bundle_i in a[student].values():
+        for bundle_j in a[other_student].values():
             if instance.agent_bundle_value(student, bundle_j) > instance.agent_bundle_value(student, bundle_i):
-                result.append((idx_i, idx_j))
+                result.append((bundle_i, bundle_j))
     return result
 
 
-def get_envy_constraints(instance, initial_budgets, a, x):
+def get_envy_constraints(instance, initial_budgets, a):
     """
         This function checks for every two students if there is envy between them,
         in case there is a constraint required for the model.
@@ -43,25 +47,26 @@ def get_envy_constraints(instance, initial_budgets, a, x):
         ...     item_capacities={"x":1, "y":1, "z":2})
         >>> initial_budgets = {"Alice": 5, "Bob": 4}
         >>> a = {'Alice': {3.5: ('x', 'y'), 3: ('x', 'z')}, 'Bob': {3.5: ('x', 'y'), 2: ('y', 'z')}}
-        >>> x =
-        >>> get_envy_constraints(different_budgets, prices, instance)
-        [[('x', 'z'), ('x', 'y')], [('y', 'z'), ('x', 'y')]]
+        >>> get_envy_constraints(instance, initial_budgets, a)
+        [(('Alice', ('x', 'z')), ('Bob', ('x', 'y')))]
     """
-    # TODO - ask erel how to do tests because we need to pass model
-    # todo pass it like array of constraints
+
     students_names = instance.agents
     envy_constraints = []
 
-    for idx_student, student in enumerate(students_names):
-        for idx_other, other_student in enumerate(students_names):
+    for student in students_names:
+        for other_student in students_names:
             if student is not other_student:
-                if initial_budgets[idx_student] > initial_budgets[idx_other]:  # check envy
+                if initial_budgets[student] > initial_budgets[other_student]:  # check envy
                     # result contain the index of the bundles that student envious other_student
-                    result = check_envy(instance, student, idx_student, other_student, idx_other, a)
+                    result = check_envy(instance, student, other_student, a)
+
                     if result:
                         for pair in result:
                             i, j = pair
-                            envy_constraints.append((x[student][i], x[other_student][j]))
+                            logger.info(f"bundle {i} , bundle {j}")
+                            # envy_constraints.append((x[student, i], x[other_student, j]))
+                            envy_constraints.append(((student, i), (other_student, j)))
                             logger.info(f"student {student} bundle {i} envy student {other_student} bundle {j}")
     return envy_constraints
 
@@ -83,54 +88,53 @@ def optimize_model(a: dict, instance: Instance, prices: dict, t: Enum, initial_b
         [[('x', 'z'), ('x', 'y')], [('y', 'z'), ('x', 'y')]]
     """
     model = Model("allocations")
-    n = len(a)  # Number of students
-    courses_names = list(instance.items)  # keys of courses
-    m = len(courses_names)  # Number of courses
-    k = {student: len(a[student].keys()) for student in a.keys()}  # Number of bundles for each student
+    courses_names = list(instance.items)
+    students_names = list(instance.agents)
 
     # Decision variables
-    # x = {(i, l): model.add_var(var_type=BINARY) for i in range(n) for l in k[i]}
-    x = {(i, l): model.add_var(var_type=BINARY) for i, num_bundles in k.items() for l in range(num_bundles)}
+    x = {(student, bundle): model.add_var(var_type=BINARY) for student in students_names for bundle in a[student].values()}
 
-    # z = [model.add_var(var_type=CONTINUOUS, lb=-instance.item_capacity[course]) for course in courses_names]
-    z = {course: model.add_var(var_type=CONTINUOUS, lb=-instance.item_capacity[course]) for course in courses_names}
-    y = {j: model.add_var(var_type=CONTINUOUS) for j in range(m)}
+    z = {course: model.add_var(var_type=CONTINUOUS, lb=-instance.item_capacity(course)) for course in courses_names}
+    y = {course: model.add_var(var_type=CONTINUOUS) for course in courses_names}
 
     # Objective function
-    objective_expr = xsum(y[j] for j in range(m))
+    objective_expr = xsum(y[course] for course in courses_names)
     model.objective = minimize(objective_expr)
 
     # Add constraints for absolute value of excess demand
-    for j in range(m):
-        model += y[j] >= z[j]
-        model += y[j] >= -z[j]
+    for course in courses_names:
+        model += y[course] >= z[course]
+        model += y[course] >= -z[course]
 
-        # Course allocation constraints
-        for course in courses_names:
-            # constraint 1: ∑  ∑(𝑥_𝑖ℓ · 𝑎_𝑖ℓ𝑗) = 𝑐_𝑗 + 𝑧_𝑗  ∀𝑗 ∈ [𝑚], 𝑝_𝑗 > 0
-            #            𝑖∈[𝑛] ℓ ∈ [𝑘_𝑖]
-            if prices[course] > 0:
-                model += xsum(x[i, l] * a[i][l].get(course, 0) for i in range(n) for l in range(k[i])) == \
-                         instance.item_capacity[course] + z[course]
-            # constraint 2: ∑     ∑(𝑥_𝑖ℓ · 𝑎_𝑖ℓ𝑗) ≤ 𝑐𝑗 + 𝑧𝑗 ∀𝑗 ∈ [𝑚], 𝑝𝑗 = 0
-            #  𝑖∈[𝑛] ℓ∈[𝑘_𝑖]
-            else:
-                model += xsum(x[i, l] * a[i][l].get(course, 0) for i in range(n) for l in range(k[i])) <= \
-                         instance.item_capacity[course] + z[course]
+    # Course allocation constraints
+    for course in courses_names:
+        # constraint 1: ∑  ∑(𝑥_𝑖ℓ · 𝑎_𝑖ℓ𝑗) = 𝑐_𝑗 + 𝑧_𝑗  ∀𝑗 ∈ [𝑚], 𝑝_𝑗 > 0
+        #            𝑖∈[𝑛] ℓ ∈ [𝑘_𝑖]
+        if prices[course] > 0:
+            model += xsum(
+                x[student, bundle] * (1 if course in bundle else 0) for student in students_names for bundle in
+                a[student].values()) == instance.item_capacity(course) + z[course]
+
+        # constraint 2: ∑     ∑(𝑥_𝑖ℓ · 𝑎_𝑖ℓ𝑗) ≤ 𝑐𝑗 + 𝑧𝑗 ∀𝑗 ∈ [𝑚], 𝑝𝑗 = 0
+        #  𝑖∈[𝑛] ℓ∈[𝑘_𝑖]
+        else:
+            model += xsum(
+                x[student, bundle] * (1 if course in bundle else 0) for student in students_names for bundle in
+                a[student].values()) <= instance.item_capacity(course) + z[course]
 
     # constraint 3: ∑𝑥_𝑖ℓ = 1  ∀𝑖 ∈ [𝑛]
     #               ℓ∈[𝑘_𝑖]
-    for i in range(n):
-        model += xsum(x[i, l] for l in range(k[i])) == 1
+    for student in students_names:
+        model += xsum(x[student, bundle] for bundle in a[student].values()) == 1
 
     # Add EF-TB constraints based on parameter t
     if t == EFTBStatus.NO_EF_TB:
         pass  # No EF-TB constraints, no need to anything
     elif t == EFTBStatus.EF_TB:
         # Add EF-TB constraints here
-        envy_constraints = get_envy_constraints(instance, initial_budgets, a, x)
+        envy_constraints = get_envy_constraints(instance, initial_budgets, a)
         for constraint in envy_constraints:
-            model += constraint[0] + constraint[1] <= 1
+            model += x[constraint[0]] + x[constraint[1]] <= 1
 
     elif t == EFTBStatus.CONTESTED_EF_TB:
         # Add contested EF-TB constraints here
@@ -142,11 +146,11 @@ def optimize_model(a: dict, instance: Instance, prices: dict, t: Enum, initial_b
     # Process and print results
     if model.num_solutions:
         print("Objective Value:", model.objective_value)
-        for i in range(n):
-            for l in range(k[i]):
-                print(f"x_{i}{l} =", x[i, l].x)
-        for j in range(m):
-            print(f"|z_{j}|=y_{j} =", y[j].x)
+        for student in students_names:
+            for l in a[student].values():
+                print(f"x_{student}{l} =", x[student, l].x)
+        for course in courses_names:
+            print(f"|z_{course}|=y_{course} =", y[course].x)
     else:
         print("Optimization was not successful. Status:", model.status)
 
