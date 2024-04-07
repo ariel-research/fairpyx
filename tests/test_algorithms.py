67f8aa232dd76ad3e66659ed5e7a00e8b3fca600
Abodi Massarwa <abodi.msarwi@gmail.com>
2024-04-07 15:07:02 +0300

    improved functionality , renamed tests

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,5 +1,7 @@
 import pytest
 from fairpyx.instances import Instance
+from fairpyx.algorithms import picking_sequence
+from fairpyx import divide
 import numpy as np
 import pytest
 
@@ -13,7 +15,7 @@ def random_uniform_extended(num_of_agents: int, num_of_items: int,
                             normalized_sum_of_values: int,
                             agent_name_template="s{index}", item_name_template="c{index}",
                             random_seed: int = None,
-                            ):
+                            equal_capacities: bool = False):
     inst = Instance(
         valuations={"Agent1": {'item1': 1}})  # helping varialbe (for the sake of calling inner # non-static-methods)
     result_instance = inst.random_uniform(num_of_agents=
@@ -31,61 +33,26 @@ def random_uniform_extended(num_of_agents: int, num_of_items: int,
     # TODO make ranmdomize categories
     category_string_template = "Category:{cat}"
     categories = {category_string_template.format(cat=cat): [] for cat in range(num_of_categories)}
-    agent_capacities_2d = {
-        agent: {category: np.random.randint(agent_capacity_bounds[0], agent_capacity_bounds[1] + 1) for category in
-                categories} for agent
-        in result_instance.agents}
-
+    if equal_capacities:
+        agent_capacities_2d = {
+            agent: {category: np.random.randint(agent_capacity_bounds[0], agent_capacity_bounds[1] + 1) for category in
+                    categories} for agent
+            in result_instance.agents}
+    else:
+        random_capacity=np.random.randint(agent_capacity_bounds[0], agent_capacity_bounds[1] + 1)
+        agent_capacities_2d = {
+            agent: {category: random_capacity for category in
+                    categories} for agent
+            in result_instance.agents}
     # print(f"and categories are :{categories}\n and agent capacities are : {agent_capacities_2d}") # TODO remove after finish
     for item in result_instance.items:
         random_category = np.random.choice(list(categories.keys()))
         categories[random_category].append(item)
 
-    return Instance, agent_capacities_2d, categories
+    return result_instance, agent_capacities_2d, categories
 
 
-def test_1():
-    print(main())
-    assert False
-
-
-def test_2():
-    print(main())
-    assert False
-
-
-def test_3():
-    print(main())
-    assert False
-
-
-def test_4():
-    print(main())
-    assert False
-
-
-def test_5():
-    print(main())
-    assert False
-
-
-def test_6():
-    print(main())
-    assert False
-
-
-def test_7():
-    print(main())
-    assert False
-
-
-def test_8():
-    print(main())
-    assert False
-
-
-# @pytest.fixture
-def main():  # todo add randomization for arguments .
+def random_instance(equal_capacities):  # todo add randomization for arguments .
     random_num_of_agents = np.random.randint(1, 10 + 1)
     random_num_of_items = np.random.randint(1, 10 + 1)
     random_num_of_categories = np.random.randint(1, random_num_of_items + 1)
@@ -95,9 +62,54 @@ def main():  # todo add randomization for arguments .
         agent_capacity_bounds=[1, 20], item_capacity_bounds=[1, 50],
         item_base_value_bounds=[1, 200], item_subjective_ratio_bounds=[0.5, 1.5],
         agent_name_template="agent{index}", item_name_template="item{index}",
-        normalized_sum_of_values=1000)
+        normalized_sum_of_values=1000, equal_capacities=equal_capacities
+    )
     return random_instance
 
 
+# TODO the only logical way to test random instances , is either
+# 1) time-complexity-based tests
+# 2) validate Envy-freeness up to 1 good in worst case scenario
+def is_fef1(alloc: dict) -> bool:
+    return True
+
+
+def test_algorithm_1():
+    instance, agent_capacities_2d, categories = random_instance(equal_capacities=True)
+    assert is_fef1(divide(algorithm=picking_sequence.per_category_round_robin, instance=instance,
+                          item_categories=categories, agent_category_capacities=agent_capacities_2d)) is True
+
+
+def test_algorithm_2():
+    instance, agent_capacities_2d, categories = random_instance()
+    assert is_fef1(divide(algorithm=picking_sequence.capped_round_robin, instance=instance,
+                          item_categories=categories, agent_category_capacities=agent_capacities_2d)) is True
+
+
+def test_algorithm_3():
+    print(random_instance())
+    assert False
+
+
+def test_algorithm_4():
+    print(random_instance())
+    assert False
+
+
+def test_algorithm_5():
+    print(random_instance())
+    assert False
+
+
+def test_algorithm_6():
+    print(random_instance())
+    assert False
+
+
+def test_algorithm_7():
+    print(random_instance())
+    assert False
+
+
 if __name__ == "__main__":
-    main()
+    random_instance()
