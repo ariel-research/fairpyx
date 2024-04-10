0df80f438d9cd152300f6a9b33e74092eb3fdae9
Abodi Massarwa <abodi.msarwi@gmail.com>
2024-04-10 23:13:41 +0300

    added Example 4 for algorithm 2

--- fairpyx/algorithms/heterogeneous_matroid_constraints_algorithms.py
+++ fairpyx/algorithms/heterogeneous_matroid_constraints_algorithms.py
@@ -23,7 +23,7 @@ def per_category_round_robin(alloc: AllocationBuilder, item_categories: dict, ag
     >>> item_categories = {'c1': ['m1', 'm2'], 'c2': ['m3']}
     >>> agent_category_capacities = {'Agent1': {'c1': 2, 'c2': 2}, 'Agent2': {'c1': 2, 'c2': 2}}
     >>> valuations = {'Agent1':{'m1':2,'m2':8,'m3':7},'Agent2':{'m1':2,'m2':8,'m3':1}}
-    >>> divide(algorithm=per_category_round_robin,instance=Instance(valuations=valuations,items=items),item_categories=item_categories,agent_category_capacities= agent_category_capacities)
+    >>> divide(algorithm=per_category_round_robin,instance=Instance(valuations=valuations,items=items),item_categories=item_categories,agent_category_capacities= agent_category_capacities,order = order)
     >>>{'Agent1':['m1','m3'],'Agent2':['m2']}
 
     >>> # Example 2
@@ -33,7 +33,7 @@ def per_category_round_robin(alloc: AllocationBuilder, item_categories: dict, ag
     >>> item_categories = {'c1': ['m1', 'm2','m3']}
     >>> agent_category_capacities = {'Agent1': {'c1':3}, 'Agent2': {'c1':3},'Agent3': {'c1':3}}
     >>> valuations = {'Agent1':{'m1':5,'m2':6,'m3':5},'Agent2':{'m1':6,'m2':5,'m3':6},'Agent3':{'m1':5,'m2':6,'m3':5}}
-    >>> divide(algorithm=per_category_round_robin,instance=Instance(valuations=valuations,items=items),item_categories=item_categories,agent_category_capacities= agent_category_capacities)
+    >>> divide(algorithm=per_category_round_robin,instance=Instance(valuations=valuations,items=items),item_categories=item_categories,agent_category_capacities= agent_category_capacities,order=order)
     >>> {'Agent1':['m2'],'Agent2':['m1'],'Agent3':['m3']}
 
 
@@ -42,9 +42,9 @@ def per_category_round_robin(alloc: AllocationBuilder, item_categories: dict, ag
     >>> order=[1,2,3,4]
     >>> items=['m1','m2','m3','m4']
     >>> item_categories = {'c1': ['m1', 'm2','m3'],'c2':['m4']}
-    >>> agent_category_capacities = {'Agent1': {'c1':1,'c2':1}, 'Agent2': {'c1':1,'c2':1},'Agent3': {'c1':1,'c2':1}}
+    >>> agent_category_capacities = {'Agent1': {'c1':3,'c2':2}, 'Agent2': {'c1':3,'c2':2},'Agent3': {'c1':3,'c2':2}} # in the papers its written capacity=size(catergory)
     >>> valuations = {'Agent1':{'m1':5,'m2':6,'m3':5},'Agent2':{'m1':6,'m2':5,'m3':6},'Agent3':{'m1':5,'m2':6,'m3':5}}
-    >>> divide(algorithm=per_category_round_robin,instance=Instance(valuations=valuations,items=items),item_categories=item_categories,agent_category_capacities= agent_category_capacities)
+    >>> divide(algorithm=per_category_round_robin,instance=Instance(valuations=valuations,items=items),item_categories=item_categories,agent_category_capacities= agent_category_capacities,order=order)
     >>> {'Agent1':['m1'],'Agent2':['m2'],'Agent3':['m3'],'Agent4':['m4']} #TODO ask Erel if i should take treat it as this or each allocation categorized for each agent ? like{'Agent1':{'c1':['m1'}...}....}
     """
 
@@ -70,7 +70,7 @@ def capped_round_robin(alloc: AllocationBuilder, item_categories: dict, agent_ca
         >>> item_categories = {'c1': ['m1']}
         >>> agent_category_capacities = {'Agent1': {'c1':0}, 'Agent2': {'c1':1}}
         >>> valuations = {'Agent1':{'m1':0},'Agent2':{'m1':420}}
-        >>> divide(algorithm=per_category_round_robin,instance=Instance(valuations=valuations,items=items),item_categories=item_categories,agent_category_capacities= agent_category_capacities)
+        >>> divide(algorithm=per_category_round_robin,instance=Instance(valuations=valuations,items=items),item_categories=item_categories,agent_category_capacities= agent_category_capacities,order = order)
         >>>{'Agent1':None,'Agent2':['m1']}
 
         >>> # Example 2 (3 agents , 4 items)
@@ -80,7 +80,7 @@ def capped_round_robin(alloc: AllocationBuilder, item_categories: dict, agent_ca
         >>> item_categories = {'c1': ['m1', 'm2','m3','m4']}
         >>> agent_category_capacities = {'Agent1': {'c1':2}, 'Agent2': {'c1':2},'Agent3': {'c1':2}}
         >>> valuations = {'Agent1':{'m1':1,'m2':1,'m3':1},'Agent2':{'m1':1,'m2':1,'m3':1},'Agent3':{'m1':1,'m2':1,'m3':1}}
-        >>> divide(algorithm=per_category_round_robin,instance=Instance(valuations=valuations,items=items),item_categories=item_categories,agent_category_capacities= agent_category_capacities)
+        >>> divide(algorithm=per_category_round_robin,instance=Instance(valuations=valuations,items=items),item_categories=item_categories,agent_category_capacities= agent_category_capacities,order=order)
         >>> {'Agent1':['m1','m4'],'Agent2':['m2'],'Agent3':['m3']}
 
 
@@ -91,8 +91,19 @@ def capped_round_robin(alloc: AllocationBuilder, item_categories: dict, agent_ca
         >>> item_categories = {'c1': ['m1', 'm2']}
         >>> agent_category_capacities = {'Agent1': {'c1':1}, 'Agent2': {'c1':1},'Agent3': {'c1':1}}
         >>> valuations = {'Agent1':{'m1':10,'m2':5},'Agent2':{'m1':5,'m2':10}}
-        >>> divide(algorithm=per_category_round_robin,instance=Instance(valuations=valuations,items=items),item_categories=item_categories,agent_category_capacities= agent_category_capacities)
+        >>> divide(algorithm=per_category_round_robin,instance=Instance(valuations=valuations,items=items),item_categories=item_categories,agent_category_capacities= agent_category_capacities,order = order)
         >>> {'Agent1':['m1'],'Agent2':['m2']}
+
+        >>> # Example 4  (4 Agents , different capacities ,extra unallocated item at the termination of the algorithm)
+        >>> from fairpyx import  divide
+        >>> order=[1,2,3,4]
+        >>> items=['m1','m2','m3','m4','m5','m6','m7']
+        >>> item_categories = {'c1': ['m1', 'm2']}
+        >>> agent_category_capacities = {'Agent1': {'c1':0}, 'Agent2': {'c1':1},'Agent3': {'c1':2},'Agent4': {'c1':3}}
+        >>> valuations = {'Agent1':{'m1':1,'m2':2,'m3':3,'m4':4,'m5':5,'m6':6,'m7':0},'Agent2':{'m1':6,'m2':5,'m3':4,'m4':3,'m5':2,'m6':1,'m7':0},'Agent3':{'m1':1,'m2':2,'m3':5,'m4':6,'m5':3,'m6':4,'m7':0},'Agent4':{'m1':5,'m2':4,'m3':1,'m4':2,'m5':3,'m6':6,'m7':0}}
+        >>> divide(algorithm=per_category_round_robin,instance=Instance(valuations=valuations,items=items),item_categories=item_categories,agent_category_capacities= agent_category_capacities,order=order)
+        >>> {'Agent1':[],'Agent2':['m1'],'Agent3':['m3','m4'],'Agent4':['m2','m5','m6']}
+
         """
     pass
 #TODO finish to algorithm 5 by 15/4
