4b85078036d2572735424b1e30dd95e30add7af5
MoriyaEster <moriyaeo100@gmail.com>
2024-06-04 19:09:21 +0300

    logger for all and outer functions

--- fairpyx/algorithms/Optimization_based_Mechanisms/OC.py
+++ fairpyx/algorithms/Optimization_based_Mechanisms/OC.py
@@ -30,25 +30,6 @@ def numberOfCourses(constraints, var, alloc, less_then):
         else:
             constraints.append(cp.sum(var[:, i]) <= less_then[student])
 
-def alloctions(alloc, var, logger):
-    # Extract the optimized values of x
-    x_values = var.value
-    logger.info("x_values - the optimum allocation: %s", x_values)
-
-    # Initialize a dictionary where each student will have an empty list
-    assign_map_courses_to_student = {student: [] for student in alloc.remaining_agents()}
-
-    # Iterate over students and courses to populate the lists
-    for i, student in enumerate(alloc.remaining_agents()):
-        for j, course in enumerate(alloc.remaining_items()):
-            if x_values[j, i] == 1:
-                assign_map_courses_to_student[student].append(course)
-
-    # Assign the courses to students based on the dictionary
-    for student, courses in assign_map_courses_to_student.items():
-        for course in courses:
-            alloc.give(student, course)
-
 
 def OC_function(alloc: AllocationBuilder, explanation_logger: ExplanationLogger = ExplanationLogger()):
     """
@@ -133,7 +114,22 @@ def OC_function(alloc: AllocationBuilder, explanation_logger: ExplanationLogger 
 
     # Check if the optimization problem was successfully solved
     if result_Z2 is not None:
-        alloctions(alloc, x, logger)
+        # Extract the optimized values of x
+        x_values = x.value
+
+        # Initialize a dictionary where each student will have an empty list
+        assign_map_courses_to_student = {student: [] for student in alloc.remaining_agents()}
+
+        # Iterate over students and courses to populate the lists
+        for j, student in enumerate(alloc.remaining_agents()):
+            for i, course in enumerate(alloc.remaining_items()):
+                if x_values[i, j] == 1:
+                    assign_map_courses_to_student[student].append(course)
+
+        # Assign the courses to students based on the dictionary
+        for student, courses in assign_map_courses_to_student.items():
+            for course in courses:
+                alloc.give(student, course)
 
         optimal_value = problem.value
         explanation_logger.info("Optimal Objective Value:", optimal_value)
