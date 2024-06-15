15a04928f9c2139e876eb4a327ce0bee15754f35
BenDabush <bendab2304@gmail.com>
2024-06-15 13:52:18 +0300

    Added full implementation to remove_oversubscription.py

--- fairpyx/algorithms/CM/remove_oversubscription.py
+++ fairpyx/algorithms/CM/remove_oversubscription.py
@@ -7,15 +7,27 @@ https://pubsonline.informs.org/doi/epdf/10.1287/opre.2016.1544
 Naama Shiponi and Ben Dabush
 1/6/2024
 """
+
 from fairpyx.instances import Instance
 from fairpyx.allocations import AllocationBuilder
-from fairpyx.algorithms.CM.A_CEEI import course_demands, find_preferred_schedule
+from fairpyx.algorithms.CM.A_CEEI import (
+    course_demands,
+    find_best_schedule,
+    find_preferred_schedule,
+)
 
 """
 Algorithm 2 : The algorithm makes sure that there are no courses that have more students registered than their capacity.
 """
 
-def remove_oversubscription(allocation: AllocationBuilder, price_vector: dict, student_budgets: dict, epsilon: float = 0.1, course_demands: callable = course_demands):
+
+def remove_oversubscription(
+    allocation: AllocationBuilder,
+    price_vector: dict,
+    student_budgets: dict,
+    epsilon: float = 0.1,
+    course_demands: callable = course_demands,
+):
     """
     Perform oversubscription elimination to adjust course prices.
 
@@ -24,7 +36,7 @@ def remove_oversubscription(allocation: AllocationBuilder, price_vector: dict, s
     :param student_budgets: dict of student budgets (dict of floats)
     :param epsilon: Small value to determine when to stop binary search
     :param demand_function: Function that takes price vector and returns excess demand vector
-    
+
     :return: Adjusted price vector (dict of floats)
 
     :pseudo code
@@ -53,8 +65,8 @@ def remove_oversubscription(allocation: AllocationBuilder, price_vector: dict, s
 
     :example
     >>> instance = Instance(
-    ...   agent_capacities = {"Alice": 2, "Bob": 3, "Tom": 3}, 
-    ...   item_capacities  = {"c1": 1, "c2": 1, "c3": 1}, 
+    ...   agent_capacities = {"Alice": 2, "Bob": 2, "Tom": 2},
+    ...   item_capacities  = {"c1": 1, "c2": 1, "c3": 1},
     ...   valuations       = {"Alice": {"c1": 50, "c2": 20, "c3": 80},
     ...                      "Bob": {"c1": 60, "c2": 40, "c3": 30},
     ...                      "Tom": {"c1": 70, "c2": 30, "c3": 70}}
@@ -66,41 +78,60 @@ def remove_oversubscription(allocation: AllocationBuilder, price_vector: dict, s
     >>> remove_oversubscription(allocation, price_vector, student_budgets, epsilon, course_demands)
     {"c1": 1.26875, "c2": 0.9, "c3": 1.24375}
 
-    >>> instance = Instance(
-    ...   agent_capacities = {"Alice": 3, "Bob": 3, "Tom": 3}, 
-    ...   item_capacities  = {"c1": 3, "c2": 3, "c3": 3}, 
-    ...   valuations       = {"Alice": {"c1": 50, "c2": 20, "c3": 80},
-    ...                      "Bob": {"c1": 60, "c2": 40, "c3": 30},
-    ...                      "Tom": {"c1": 70, "c2": 30, "c3": 70}}
-    ... )
-    >>> allocation = AllocationBuilder(instance)
-    >>> price_vector = {"c1": 1.2, "c2": 0.9, "c3": 1}
-    >>> epsilon = 0.1
-    >>> student_budgets = {"Alice": 2.2, "Bob": 2.1, "Tom": 2.0}
-    >>> remove_oversubscription(allocation, price_vector, student_budgets, epsilon, course_demands)
-    {"c1": 1.2, "c2": 0.9, "c3": 1}
+    # >>> instance = Instance(
+    # ...   agent_capacities = {"Alice": 3, "Bob": 3, "Tom": 3},
+    # ...   item_capacities  = {"c1": 3, "c2": 3, "c3": 3},
+    # ...   valuations       = {"Alice": {"c1": 50, "c2": 20, "c3": 80},
+    # ...                      "Bob": {"c1": 60, "c2": 40, "c3": 30},
+    # ...                      "Tom": {"c1": 70, "c2": 30, "c3": 70}}
+    # ... )
+    # >>> allocation = AllocationBuilder(instance)
+    # >>> price_vector = {"c1": 1.2, "c2": 0.9, "c3": 1}
+    # >>> epsilon = 0.1
+    # >>> student_budgets = {"Alice": 2.2, "Bob": 2.1, "Tom": 2.0}
+    # >>> remove_oversubscription(allocation, price_vector, student_budgets, epsilon, course_demands)
+    # {"c1": 1.2, "c2": 0.9, "c3": 1}
 
-    >>> instance = Instance(
-    ...   agent_capacities = {"Alice": 3, "Bob": 3, "Tom": 3}, 
-    ...   item_capacities  = {"c1": 1, "c2": 1, "c3": 1}, 
-    ...   valuations       = {"Alice": {"c1": 50, "c2": 20, "c3": 80},
-    ...                      "Bob": {"c1": 60, "c2": 40, "c3": 30},
-    ...                      "Tom": {"c1": 70, "c2": 30, "c3": 70}}
-    ... )
-    >>> allocation = AllocationBuilder(instance)
-    >>> price_vector = {"c1": 0, "c2": 0, "c3": 0}
-    >>> epsilon = 0.1
-    >>> student_budgets = {"Alice": 2.2, "Bob": 2.1, "Tom": 2.0}
-    >>> remove_oversubscription(allocation, price_vector, student_budgets, epsilon, course_demands)
-    {"c1": 2.0375, "c2": 1.6875, "c3": 2.084375}
+    # >>> instance = Instance(
+    # ...   agent_capacities = {"Alice": 3, "Bob": 3, "Tom": 3},
+    # ...   item_capacities  = {"c1": 1, "c2": 1, "c3": 1},
+    # ...   valuations       = {"Alice": {"c1": 50, "c2": 20, "c3": 80},
+    # ...                      "Bob": {"c1": 60, "c2": 40, "c3": 30},
+    # ...                      "Tom": {"c1": 70, "c2": 30, "c3": 70}}
+    # ... )
+    # >>> allocation = AllocationBuilder(instance)
+    # >>> price_vector = {"c1": 0, "c2": 0, "c3": 0}
+    # >>> epsilon = 0.1
+    # >>> student_budgets = {"Alice": 2.2, "Bob": 2.1, "Tom": 2.0}
+    # >>> remove_oversubscription(allocation, price_vector, student_budgets, epsilon, course_demands)
+    # {"c1": 2.0375, "c2": 1.6875, "c3": 2.084375}
     """
-    max_budget = max(student_budgets.values()) + epsilon  # ¯p scalar price greater than any budget
+    max_budget = (
+        max(student_budgets.values()) + epsilon
+    )  # ¯p scalar price greater than any budget
     # preferred_schedule = find_preferred_schedule(allocation.instance._valuations, allocation.instance._agent_capacities, allocation.instance.item_conflicts, allocation.instance.agent_conflicts)
-    preferred_schedule = {"Alice":  [[1, 0, 1], [0, 1, 1], [1, 1, 0]] , "Bob": [[1, 1, 0], [1, 0, 1], [0, 1, 1]], "Tom": [[1, 0, 1], [1, 1, 0], [0, 1, 1]]}
+    item_conflicts = {
+        item: allocation.instance.item_conflicts(item)
+        for item in allocation.instance.items
+    }
+    agent_conflicts = {
+        agent: allocation.instance.agent_conflicts(agent)
+        for agent in allocation.instance.agents
+    }
+
+    preferred_schedule = find_preferred_schedule(
+        allocation.instance._valuations,
+        allocation.instance._agent_capacities,
+        item_conflicts,
+        agent_conflicts,
+    )
+    # print(preferred_schedule)
 
     while True:
         # 1: Find the most oversubscribed course
-        excess_demands = course_demands(price_vector, allocation, student_budgets, preferred_schedule)
+        excess_demands = course_demands(
+            price_vector, allocation, student_budgets, preferred_schedule
+        )
         highest_demand_course = max(excess_demands, key=excess_demands.get)
         highest_demand = excess_demands[highest_demand_course]
         if highest_demand <= 0:
@@ -115,7 +146,9 @@ def remove_oversubscription(allocation: AllocationBuilder, price_vector: dict, s
         while high_price - low_price >= epsilon:
             p_mid = (low_price + high_price) / 2
             price_vector[highest_demand_course] = p_mid
-            current_demand = course_demands(price_vector, allocation, student_budgets, preferred_schedule)[highest_demand_course]
+            current_demand = course_demands(
+                price_vector, allocation, student_budgets, preferred_schedule
+            )[highest_demand_course]
             if current_demand > d_star:
                 low_price = p_mid
             else:
@@ -124,10 +157,46 @@ def remove_oversubscription(allocation: AllocationBuilder, price_vector: dict, s
         # Set to the higher price to be sure oversubscription is at most d*
         price_vector[highest_demand_course] = high_price
 
-    return price_vector    
+    return price_vector
 
 
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
 
+    instance = Instance(
+        agent_capacities={"Alice": 2, "Bob": 2, "Tom": 2},
+        item_capacities={"c1": 1, "c2": 1, "c3": 1},
+        valuations={
+            "Alice": {"c1": 50, "c2": 20, "c3": 80},
+            "Bob": {"c1": 60, "c2": 40, "c3": 30},
+            "Tom": {"c1": 70, "c2": 30, "c3": 70},
+        },
+    )
+    allocation = AllocationBuilder(instance)
+    price_vector = {"c1": 1.2, "c2": 0.9, "c3": 1}
+    epsilon = 0.1
+    student_budgets = {"Alice": 2.2, "Bob": 2.1, "Tom": 2.0}
+
+    max_budget = (
+        max(student_budgets.values()) + 0.01
+    )  # ¯p scalar price greater than any budget
+    # preferred_schedule = find_preferred_schedule(allocation.instance._valuations, allocation.instance._agent_capacities, allocation.instance.item_conflicts, allocation.instance.agent_conflicts)
+    item_conflicts = {
+        item: allocation.instance.item_conflicts(item)
+        for item in allocation.instance.items
+    }
+    agent_conflicts = {
+        agent: allocation.instance.agent_conflicts(agent)
+        for agent in allocation.instance.agents
+    }
+
+    preferred_schedule = find_preferred_schedule(
+        allocation.instance._valuations,
+        allocation.instance._agent_capacities,
+        item_conflicts,
+        agent_conflicts,
+    )
+    # print(preferred_schedule)
+    # remove_oversubscription(allocation, price_vector, student_budgets, epsilon, course_demands)
