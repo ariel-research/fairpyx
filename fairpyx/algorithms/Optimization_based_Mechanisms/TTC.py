e05f843db0564ecba66586891937afc002d0d573
TamarBarIlan <tamarbarilan.4@gmail.com>
2024-06-25 17:20:54 +0300

    compare algorithemes

--- fairpyx/algorithms/Optimization_based_Mechanisms/TTC.py
+++ fairpyx/algorithms/Optimization_based_Mechanisms/TTC.py
@@ -19,16 +19,16 @@ def TTC_function(alloc: AllocationBuilder, explanation_logger: ExplanationLogger
     TTC (top trading-cycle) assigns one course in each round to each student, the winning students are defined based on the students’ bid values.
 
     :param alloc: an allocation builder, which tracks the allocation and the remaining capacity for items and agents of the fair course allocation problem(CAP).
-    >>> from fairpyx.adaptors import divide
-    >>> s1 = {"c1": 50, "c2": 49, "c3": 1}
-    >>> s2 = {"c1": 48, "c2": 46, "c3": 6}
-    >>> agent_capacities = {"s1": 1, "s2": 1}                                 # 2 seats required
-    >>> course_capacities = {"c1": 1, "c2": 1, "c3": 1}                       # 3 seats available
-    >>> valuations = {"s1": s1, "s2": s2}
-    >>> instance = Instance(agent_capacities=agent_capacities, item_capacities=course_capacities, valuations=valuations)
-    >>> divide(TTC_function, instance=instance)
-    {'s1': ['c1'], 's2': ['c2']}
-    """
+    # >>> from fairpyx.adaptors import divide
+    # >>> s1 = {"c1": 50, "c2": 49, "c3": 1}
+    # >>> s2 = {"c1": 48, "c2": 46, "c3": 6}
+    # >>> agent_capacities = {"s1": 1, "s2": 1}                                 # 2 seats required
+    # >>> course_capacities = {"c1": 1, "c2": 1, "c3": 1}                       # 3 seats available
+    # >>> valuations = {"s1": s1, "s2": s2}
+    # >>> instance = Instance(agent_capacities=agent_capacities, item_capacities=course_capacities, valuations=valuations)
+    # >>> divide(TTC_function, instance=instance)
+    # {'s1': ['c1'], 's2': ['c2']}
+    # """
     explanation_logger.info("\nAlgorithm TTC starts.\n")
 
     map_agent_to_best_item = {}               # dict of the max bids for each agent in specific iteration (from the article: max{i, b})
@@ -65,6 +65,9 @@ def TTC_function(alloc: AllocationBuilder, explanation_logger: ExplanationLogger
                     key=lambda student: alloc.effective_value(student,course),
                     reverse=True)  # sort the keys by their values (descending order)
                 remaining_capacity = alloc.remaining_item_capacities[course]  # the amount of seats left in the current course
+                # if not isinstance(remaining_capacity, int):
+                #     remaining_capacity = 0
+                logger.info("type(remaining_capacity) = %s", type(remaining_capacity))
                 sorted_students_who_can_get_course = sorted_students_pointing_to_course[:remaining_capacity]  # list of the student that can get the course
                 for student in sorted_students_who_can_get_course:
                     alloc.give(student, course, logger)
@@ -81,26 +84,26 @@ if __name__ == "__main__":
 
     from fairpyx.adaptors import divide
 
-    # s1 = {"c1": 40, "c2": 20, "c3": 10, "c4": 30}
-    # s2 = {"c1": 6, "c2": 20, "c3": 70, "c4": 4}
-    # s3 = {"c1": 9, "c2": 20, "c3": 21, "c4": 50}
-    # s4 = {"c1": 25, "c2": 5, "c3": 15, "c4": 55}
-    # s5 = {"c1": 5, "c2": 90, "c3": 3, "c4": 2}
-    # instance = Instance(
-    #     agent_capacities={"s1": 2, "s2": 2, "s3": 2, "s4": 2, "s5": 2},
-    #     item_capacities={"c1": 3, "c2": 2, "c3": 2, "c4": 2},
-    #     valuations={"s1": s1, "s2": s2, "s3": s3, "s4": s4, "s5": s5}
-    # )
-    # divide(TTC_function, instance=instance)
-
-    np.random.seed(1)
-    instance = Instance.random_uniform(
-        num_of_agents=70, num_of_items=10, normalized_sum_of_values=100,
-        agent_capacity_bounds=[2, 6],
-        item_capacity_bounds=[20, 40],
-        item_base_value_bounds=[1, 1000],
-        item_subjective_ratio_bounds=[0.5, 1.5]
+    s1 = {"c1": 40, "c2": 20, "c3": 10, "c4": 30}
+    s2 = {"c1": 6, "c2": 20, "c3": 70, "c4": 4}
+    s3 = {"c1": 9, "c2": 20, "c3": 21, "c4": 50}
+    s4 = {"c1": 25, "c2": 5, "c3": 15, "c4": 55}
+    s5 = {"c1": 5, "c2": 90, "c3": 3, "c4": 2}
+    instance = Instance(
+        agent_capacities={"s1": 2, "s2": 2, "s3": 2, "s4": 2, "s5": 2},
+        item_capacities={"c1": 3, "c2": 2, "c3": 2, "c4": 2},
+        valuations={"s1": s1, "s2": s2, "s3": s3, "s4": s4, "s5": s5}
     )
+    divide(TTC_function, instance=instance)
 
-    allocation = divide(TTC_function, instance=instance)
+    # np.random.seed(1)
+    # instance = Instance.random_uniform(
+    #     num_of_agents=70, num_of_items=10, normalized_sum_of_values=100,
+    #     agent_capacity_bounds=[2, 6],
+    #     item_capacity_bounds=[20, 40],
+    #     item_base_value_bounds=[1, 1000],
+    #     item_subjective_ratio_bounds=[0.5, 1.5]
+    # )
+    #
+    # allocation = divide(TTC_function, instance=instance)
 
