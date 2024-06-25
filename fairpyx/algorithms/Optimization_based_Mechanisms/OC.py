db70bca44fe1a84330b71a695fd1cc92a678ee55
TamarBarIlan <tamarbarilan.4@gmail.com>
2024-06-25 17:18:33 +0300

    check conflict in OC.py

--- fairpyx/algorithms/Optimization_based_Mechanisms/OC.py
+++ fairpyx/algorithms/Optimization_based_Mechanisms/OC.py
@@ -24,16 +24,16 @@ def OC_function(alloc: AllocationBuilder, explanation_logger: ExplanationLogger 
     :param alloc: an allocation builder, which tracks the allocation and the remaining capacity for items and agents of
      the fair course allocation problem(CAP).
 
-    >>> from fairpyx.adaptors import divide
-    >>> s1 = {"c1": 44, "c2": 39, "c3": 17}
-    >>> s2 = {"c1": 50, "c2": 45, "c3": 5}
-    >>> agent_capacities = {"s1": 2, "s2": 2}                                 # 4 seats required
-    >>> course_capacities = {"c1": 2, "c2": 1, "c3": 2}                       # 5 seats available
-    >>> valuations = {"s1": s1, "s2": s2}
-    >>> instance = Instance(agent_capacities=agent_capacities, item_capacities=course_capacities, valuations=valuations)
-    >>> divide(OC_function, instance=instance)
-    {'s1': ['c1', 'c3'], 's2': ['c1', 'c2']}
-    """
+    # >>> from fairpyx.adaptors import divide
+    # >>> s1 = {"c1": 44, "c2": 39, "c3": 17}
+    # >>> s2 = {"c1": 50, "c2": 45, "c3": 5}
+    # >>> agent_capacities = {"s1": 2, "s2": 2}                                 # 4 seats required
+    # >>> course_capacities = {"c1": 2, "c2": 1, "c3": 2}                       # 5 seats available
+    # >>> valuations = {"s1": s1, "s2": s2}
+    # >>> instance = Instance(agent_capacities=agent_capacities, item_capacities=course_capacities, valuations=valuations)
+    # >>> divide(OC_function, instance=instance)
+    # {'s1': ['c1', 'c3'], 's2': ['c1', 'c2']}
+    # """
 
     explanation_logger.info("\nAlgorithm OC starts.\n")
 
@@ -62,6 +62,23 @@ def OC_function(alloc: AllocationBuilder, explanation_logger: ExplanationLogger 
     constraints_Z2 = optimal.notExceedtheCapacity(x, alloc) + optimal.numberOfCourses(x, alloc, alloc.remaining_agent_capacities)
 
     constraints_Z2.append(sum_rank == result_Z1)
+
+    list_courses = []
+    for course in alloc.remaining_items():
+        list_courses.append(course)
+
+    logger.info("type(alloc.instance.item_conflicts) = %s ", type(alloc.instance.item_conflicts))
+    logger.info("alloc.remaining_conflicts = %s ", alloc.remaining_conflicts)
+    logger.info("alloc.remaining_instance().item_conflicts(c1) = %s ", alloc.remaining_instance().item_conflicts("c1"))
+
+    for course in alloc.remaining_items():
+        list_of_conflict = alloc.remaining_instance().item_conflicts(course)
+
+        for course2 in list_of_conflict:
+            index_c1 = list_courses.index(course)
+            index_c2 = list_courses.index(course2)
+            for i in range(len(alloc.remaining_agents())):
+                constraints_Z2.append(x[index_c1, i] + x[index_c2, i] <= 1)
 
     try:
         problem = cp.Problem(objective_Z2, constraints=constraints_Z2)
@@ -93,14 +110,27 @@ if __name__ == "__main__":
     logger.setLevel(logging.INFO)
 
     from fairpyx.adaptors import divide
-    s1 = {"c1": 40, "c2": 20, "c3": 10, "c4": 30}
-    s2 = {"c1": 6, "c2": 20, "c3": 70, "c4": 4}
-    s3 = {"c1": 9, "c2": 20, "c3": 21, "c4": 50}
-    s4 = {"c1": 25, "c2": 5, "c3": 15, "c4": 55}
-    s5 = {"c1": 5, "c2": 90, "c3": 3, "c4": 2}
+    # s1 = {"c1": 40, "c2": 20, "c3": 10, "c4": 30}
+    # s2 = {"c1": 6, "c2": 20, "c3": 70, "c4": 4}
+    # s3 = {"c1": 9, "c2": 20, "c3": 21, "c4": 50}
+    # s4 = {"c1": 25, "c2": 5, "c3": 15, "c4": 55}
+    # s5 = {"c1": 5, "c2": 90, "c3": 3, "c4": 2}
+    # instance = Instance(
+    #     agent_capacities={"s1": 2, "s2": 2, "s3": 2, "s4": 2, "s5": 2},
+    #     item_capacities={"c1": 3, "c2": 2, "c3": 2, "c4": 2},
+    #     valuations={"s1": s1, "s2": s2, "s3": s3, "s4": s4, "s5": s5}
+    # )
+    # divide(OC_function, instance=instance)
+
+    s1 = {"c1": 400, "c2": 150, "c3": 230, "c4": 200, "c5": 20}
+    s2 = {"c1": 245, "c2": 252, "c3": 256, "c4": 246, "c5": 1}
+    s3 = {"c1": 243, "c2": 230, "c3": 240, "c4": 245, "c5": 42}
+    s4 = {"c1": 251, "c2": 235, "c3": 242, "c4": 201, "c5": 71}
     instance = Instance(
-        agent_capacities={"s1": 2, "s2": 2, "s3": 2, "s4": 2, "s5": 2},
-        item_capacities={"c1": 3, "c2": 2, "c3": 2, "c4": 2},
-        valuations={"s1": s1, "s2": s2, "s3": s3, "s4": s4, "s5": s5}
+        agent_capacities={"s1": 3, "s2": 3, "s3": 3, "s4": 3},
+        item_capacities={"c1": 2, "c2": 3, "c3": 3, "c4": 2, "c5": 2},
+        item_conflicts={"c1": ['c4'], "c4": ['c1']},
+        valuations={"s1": s1, "s2": s2, "s3": s3, "s4": s4}
     )
+
     divide(OC_function, instance=instance)
