f34ec2ba7eee3be2296a6476e47ebffdd9a26bfa
Abodi Massarwa <abodi.msarwi@gmail.com>
2024-07-02 23:24:10 +0300

    removed unused rows

--- experiments/compare_heterogeneous_matroid_constraints_algorithms.py
+++ experiments/compare_heterogeneous_matroid_constraints_algorithms.py
@@ -52,22 +52,9 @@ def compare_heterogeneous_matroid_constraints_algorithms_egalitarian_utilitarian
         'algorithm': [
                       per_category_capped_round_robin]
     }
-    # input_ranges_4 = {
-    #     'equal_capacities': [True],
-    #     'equal_valuations': [False,True],
-    #     'binary_valuations': [False],
-    #     'num_of_items': range(10,20),
-    #     'category_count': [2],
-    #     'item_capacity_bounds': range(1, 1 + 1),
-    #     'random_seed_num': [0],
-    #     'num_of_agents': range(10,20),
-    #     'algorithm': [capped_round_robin,
-    #                    two_categories_capped_round_robin]
-    # }
     expr.run_with_time_limit(run_experiment,input_ranges_1,5)
     expr.run_with_time_limit(run_experiment, input_ranges_2, 5)
     expr.run_with_time_limit(run_experiment, input_ranges_3, 5)
-    #expr.run_with_time_limit(run_experiment, input_ranges_4, 5)
 def run_experiment(equal_capacities:bool,equal_valuations:bool,binary_valuations:bool,category_count:int,item_capacity_bounds:int,random_seed_num:int,num_of_agents:int,algorithm:callable,num_of_items:int):
     # Mapping of algorithms to their specific argument sets
     algo_args = {
@@ -94,19 +81,18 @@ def run_experiment(equal_capacities:bool,equal_valuations:bool,binary_valuations
 
     # Filter kwargs to include only those required by the chosen algorithm
     filtered_kwargs = {k: v for k, v in kwargs.items() if k in required_args}
+    #Egalitarian algorithm
     valuation_matrix = [[instance.agent_item_value(agent, item) for item in instance.items] for agent in
                         instance.agents]# simply forming valuations as in matrix so we can run fractional egalitarian algorithm
     not_rounded_egal=fractional_egalitarian_allocation(Instance(valuation_matrix),normalize_utilities=False)
     epsilon = 1 # replaced because a number (0,1) isnt a good apprach since it gives a very large number when being divided by
-    min_egalitarian_algorithm_value=min(not_rounded_egal)
+    min_egalitarian_algorithm_value=min(not_rounded_egal)#egalitarian value
     min_egalitarian_algorithm_value_denominator=min_egalitarian_algorithm_value if min_egalitarian_algorithm_value!=0 else min_egalitarian_algorithm_value+epsilon
-    experiments_csv.logger.info(f'valuation_matrix -> {valuation_matrix} \n and egalitarian allocation ->{not_rounded_egal} \n min value of it is -> {min_egalitarian_algorithm_value}')
-    rounded_egal=rounded_allocation(not_rounded_egal,1)# integer
+    #experiments_csv.logger.info(f'valuation_matrix -> {valuation_matrix} \n and egalitarian allocation ->{not_rounded_egal} \n min value of it is -> {min_egalitarian_algorithm_value}')
+    #running current algorithm with the appropriate arguments
     algorithm(**filtered_kwargs)
-    # bundle_values={agent:sum(instance.agent_item_value(agent,item) for item in bundle) for agent,bundle in alloc.bundles.items()}# sums the values of each agent bundle
-    # min_bundle_value=min(bundle_values.values())
-    min_algorithm_bundle_value=min(alloc.agent_bundle_value(agent,bundle) for agent,bundle in alloc.bundles.items())
-    # utilitarian algorithm
+    min_algorithm_bundle_value=min(alloc.agent_bundle_value(agent,bundle) for agent,bundle in alloc.bundles.items())# to compare with egalitarian algorithm
+    # Utilitarian algorithm
     alloc_utilitarian=AllocationBuilder(instance)
     utilitarian_matching(alloc_utilitarian)
     utilitarian_bundle_sum=sum(alloc_utilitarian.agent_bundle_value(agent,bundle)for agent,bundle in alloc_utilitarian.bundles.items())
@@ -120,17 +106,10 @@ def run_experiment(equal_capacities:bool,equal_valuations:bool,binary_valuations
     return {'egalitarian_algorithm_min_value':min_egalitarian_algorithm_value,'current_algorithm_min_value':min_algorithm_bundle_value,'ratio_egalitarian':min_algorithm_bundle_value/min_egalitarian_algorithm_value_denominator,'utilitarian_algorithm_sum':utilitarian_bundle_sum,'current_algorithm_sum':current_algorithm_bundle_sum,'ratio_utilitarian':current_algorithm_bundle_sum/utilitarian_bundle_sum_denominator}
 
 if __name__ == '__main__':
-    # not_rounded_egal = fractional_egalitarian_allocation(Instance(valuation_matrix), normalize_utilities=False)
-    # epsilon = 1  # replaced because a number (0,1) isnt a good apprach since it gives a very large number when being divided by
-    # min_egalitarian_algorithm_value = min(not_rounded_egal)
-    # min_egalitarian_algorithm_value_denominator = min_egalitarian_algorithm_value if min_egalitarian_algorithm_value != 0 else min_egalitarian_algorithm_value + epsilon
-    #
-
-
-
-    experiments_csv.logger.setLevel(logging.INFO)
+    #experiments_csv.logger.setLevel(logging.INFO)
     #compare_heterogeneous_matroid_constraints_algorithms_egalitarian_utilitarian()
     #experiments_csv.single_plot_results('results/egalitarian_utilitarian_comparison_heterogeneous_constraints_algorithms.csv',filter={},x_field='num_of_agents',y_field='ratio_egalitarian',z_field='algorithm',save_to_file='results/egalitarian_comparison_heterogeneous_constraints_algorithms.png') # egalitarian ratio plot
     #experiments_csv.single_plot_results('results/egalitarian_utilitarian_comparison_heterogeneous_constraints_algorithms.csv',filter={},x_field='num_of_agents',y_field='ratio_utilitarian',z_field='algorithm',save_to_file='results/utilitarian_comparison_heterogeneous_constraints_algorithms.png') # utilitarian ratio plot
     #experiments_csv.single_plot_results('results/egalitarian_utilitarian_comparison_heterogeneous_constraints_algorithms.csv',filter={},x_field='num_of_agents',y_field='runtime',z_field='algorithm',save_to_file='results/runtime_comparison_heterogeneous_constraints_algorithms.png') # runtime plot
+    pass
 
