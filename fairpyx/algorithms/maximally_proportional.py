d0040f4161f1ef5aa92a53dbc5529745a29b98be
elroicarmel <elroica277@gmail.com>
2025-05-06 22:18:08 +0300

    add: signatures + tests

---
+++ fairpyx/algorithms/maximally_proportional.py
@@ -0,0 +1,103 @@
+"""
+An implementation of the algorithm in:
+"An algorithm for the proportional division of indivisible items"
+by Brams, Steven J. and Kilgour, D. Marc and Klamler, Christian (2014),
+http://https://mpra.ub.uni-muenchen.de/56587/
+
+Programmer: Elroi Carmel
+Date: 2025-05
+"""
+
+from fairpyx import Instance, AllocationBuilder, divide
+from typing import Any, List, Iterable, Callable
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def maximally_proportional_allocation(alloc: AllocationBuilder):
+    """
+    Finds an allocation maximizing the possible number of players with proportional bundles,
+    guaranteeing the best attainable minimum rank among them.
+
+    >>> instance = Instance(valuations={"Alice":[40, 35, 25], "Bob":[35, 40, 25], "Tom":[40, 25, 35]})
+    >>> divide(maximally_proportional_allocation, instance)
+    {'Alice': [0], 'Bob': [1], 'Tom': [2]}
+    >>> valuations = [[7, 4, 33, 21, 24],
+    ...              [47, 15, 43, 21, 25],
+    ...              [10, 24, 20, 31, 5],
+    ...              [32, 33, 3, 2, 23],
+    ...              [30, 4, 1, 11, 31]]
+    >>> instance = Instance(valuations=valuations)
+    >>> divide(maximally_proportional_allocation, instance)
+    {0: [2], 1: [0], 2: [3], 3: [1], 4: [4]}
+    >>> instance = Instance(valuations=[[8,30,48,10,15],[12,7,29,15,5],[35,42,5,22,44]])
+    >>> divide(maximally_proportional_allocation, instance)
+    {0: [2], 1: [0, 3],  2: [1, 4]}
+
+    """
+    pass
+
+
+def get_minimal_bundles(alloc: AllocationBuilder, agent: Any) -> List:
+    """
+    Finds all of the agent's minimal bundles and returns them in descending
+    order by their total value.
+
+    Args:
+        alloc (AllocationBuilder): Used to retrive the items and the agent's utility
+        agent (Any): One of the agents
+
+    Returns:
+        List: agent's minimal bundle
+
+    Examples:
+    >>> val = [[35, 30, 10, 25],[25, 21, 34, 20],[16, 32, 12, 38], [91, 66, 12, 89]]
+    >>> instance = Instance(valuations=val)
+    >>> alloc = AllocationBuilder(instance)
+    >>> get_minimal_bundles(alloc, 0)
+    [[0, 1], [0, 3], [1, 3]]
+    >>> get_minimal_bundles(alloc, 1)
+    [[0, 1, 3], [0, 2], [1, 2], [2, 3]]
+    >>> get_minimal_bundles(alloc, 2)
+    [[1, 3], [0, 1, 2], [0, 3], [2, 3]]
+    >>> get_minimal_bundles(alloc, 3)
+    [[0, 3], [0, 1], [1, 3]]
+    """
+    pass
+
+
+def is_minimal_bundle(
+    bundle: Iterable, valuation_func: Callable, prop_share: float
+) -> bool:
+    """
+    A bundle is minimal for an agent if and only if it satisfies the
+    following conditions:
+    1. The agent's valuation of the bundle is >= (sum_items_value)/(num of agents) i.e. proportional.
+    2. Removing any item from the bundle makes it strictly less than proportional.
+
+    Args:
+        bundle (Iterable): items in the bundle
+        valuation_func (Callable): agent's utility function
+        prop_share (float): agnet's proportional share of utility
+
+    Returns:
+        bool: True if the bundle is minimal else False
+
+    Examples:
+    >>> is_minimal_bundle([0], [40, 35, 25], 100/3)
+    True
+    >>> is_minimal_bundle([0, 1], [40, 35, 25], 100/3)
+    False
+    >>> is_minimal_bundle([0, 1], [35, 30, 10, 25], 25)
+    True
+    >>> is_minimal_bundle([0, 1, 3], [35, 30, 10, 25], 25)
+    False
+    """
+    pass
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
---
+++ tests/test_maximally_proportional.py
@@ -0,0 +1,128 @@
+"""
+Test the maximally proportional allocation algorithm.
+
+Programmer: Elroi Carmel
+Since:  2025-05
+"""
+
+import pytest
+from fairpyx import Instance, divide, validate_allocation
+from fairpyx.algorithms import maximally_proportional_allocation
+
+
+def test_feasibility():
+    ntest = 10
+    for i in range(ntest):
+        instance = Instance.random_uniform(
+            num_of_agents=4,
+            num_of_items=6,
+            item_capacity_bounds=(1, 1),
+            agent_capacity_bounds=(1, 1),
+            item_base_value_bounds=(60, 100),
+            item_subjective_ratio_bounds=(0.6, 1.4),
+            normalized_sum_of_values=100,
+            random_seed=i,
+        )
+        allocation = divide(maximally_proportional_allocation, instance)
+        validate_allocation(allocation=allocation, instance=instance)
+
+
+def test_proportional():
+    """
+    All of the agents should accept proportional bundles
+    """
+
+    def is_proportional(allocation: dict, instance: Instance):
+        all_items = list(instance.items)
+        for agent, bundle in allocation.items():
+            agent_propor_share = (
+                instance.agent_bundle_value(agent, all_items) / instance.num_of_agents
+            )
+            if instance.agent_bundle_value(agent, bundle) < agent_propor_share:
+                return False
+        return True
+
+    instance = Instance(valuations=[[21, 49, 12, 81], [89, 63, 22, 75]])
+    alloc = divide(maximally_proportional_allocation, instance)
+    assert is_proportional(alloc, instance) == True
+
+    # random instance
+    RAND_SEED = 4
+    instance = Instance.random_uniform(
+        num_of_agents=5,
+        num_of_items=10,
+        item_capacity_bounds=(1, 1),
+        agent_capacity_bounds=(1, 1),
+        item_base_value_bounds=(20, 25),
+        item_subjective_ratio_bounds=(0.5, 3.0),
+        normalized_sum_of_values=50,
+        random_seed=RAND_SEED,
+    )
+    alloc = divide(maximally_proportional_allocation, instance)
+    assert is_proportional(alloc, instance) == True
+
+
+def test_maximallity():
+    """
+    When there are no complete proportional allocation available
+    the algorithm maximizes the amount of players which received
+    minimal bundle
+    """
+
+    def num_agents_received(alloc: dict) -> int:
+        return sum(1 for agent in alloc if len(alloc[agent]))
+
+    # Example 2 from paper
+    instance = Instance(
+        valuations=[
+            [45, 35, 10, 10],
+            [35, 45, 10, 10],
+            [45, 10, 35, 10],
+            [66, 4, 10, 20],
+        ]
+    )
+    alloc = divide(maximally_proportional_allocation, instance)
+    assert num_agents_received(alloc) == 3
+
+    # Example 3 from paper
+    instance = Instance(
+        valuations=[
+            [78, 12, 10, 2],
+            [55, 30, 10, 5],
+            [35, 30, 25, 10],
+            [48, 22, 20, 10],
+        ]
+    )
+    alloc = divide(maximally_proportional_allocation, instance)
+    assert num_agents_received(alloc) == 3
+
+    # Example 4 from paper
+
+    instance = Instance(
+        valuations=[
+            [20, 20, 31, 25, 2, 2],
+            [31, 25, 20, 20, 2, 2],
+            [5, 7, 3, 4, 80, 1],
+            [4, 4, 4, 4, 4, 80],
+        ]
+    )
+    alloc = divide(maximally_proportional_allocation, instance)
+    assert num_agents_received(alloc) == 4
+    # in this example also all items are given
+    items_given = sum(alloc.values(), [])
+    items_given.sort()
+    assert items_given == sorted(instance.items)
+
+
+def test_pareto_optimal():
+    """
+    If there are multiple complete/maximall allocations
+    the alogrithm should choose the pareto optimal one
+    """
+    instance = Instance(valuations=[[35, 40, 25], [40, 35, 25], [40, 25, 35]])
+    alloc = divide(maximally_proportional_allocation, instance)
+    assert alloc == {0: [1], 1: [0], 2: [2]}
+
+
+if __name__ == "__main__":
+    pytest.main(["-v", __file__])
