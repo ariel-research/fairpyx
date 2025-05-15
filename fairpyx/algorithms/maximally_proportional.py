3dc2a4f1ba035dc77d444ea1398c2148984a8684
elroicarmel <elroica277@gmail.com>
2025-05-15 11:10:53 +0300

    fix: get_minimal_bundles doctest

--- fairpyx/algorithms/maximally_proportional.py
+++ fairpyx/algorithms/maximally_proportional.py
@@ -59,13 +59,19 @@ def get_minimal_bundles(
     >>> instance = Instance(valuations=val)
     >>> alloc = AllocationBuilder(instance)
     >>> get_minimal_bundles(alloc, 0)
-    [[0, 1], [0, 3], [1, 3]]
+    [[0], [1], [3]]
     >>> get_minimal_bundles(alloc, 1)
-    [[0, 1, 3], [0, 2], [1, 2], [2, 3]]
+    [[1, 3], [2], [0]]
     >>> get_minimal_bundles(alloc, 2)
-    [[1, 3], [0, 1, 2], [0, 3], [2, 3]]
+    [[3], [1], [0, 2]]
     >>> get_minimal_bundles(alloc, 3)
-    [[0, 3], [0, 1], [1, 3]]
+    [[0], [3], [1]]
+    >>> val = [[15, 33, 16, 6, 26], [34, 38, 4, 16, 8]]
+    >>> alloc = AllocationBuilder(Instance(valuations=val))
+    >>> get_minimal_bundles(alloc, 0)
+    [[1, 4], [0, 2, 4], [1, 2], [0, 1], [2, 3, 4]]
+    >>> get_minimal_bundles(alloc, 1)
+    [[0, 1], [1, 3], [0, 3], [1, 2, 4]]
     """
     pass
 
