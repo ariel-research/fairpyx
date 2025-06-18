1396dc16599273c806a3425453647bf41c67db6b
TrachtmanLior <lior.trachtman@gmail.com>
2025-06-18 15:15:01 +0300

    Adjusted dependencies

--- leximin_web/app.py
+++ leximin_web/app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
+
 from flask import Flask, render_template, request, render_template_string
 from fairpyx.instances import Instance
 from fairpyx.allocations import AllocationBuilder
