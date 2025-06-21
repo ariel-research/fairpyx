dd7860181d6a2b66a5ec430b5fd224a8a9b12034
shakedS10 <155729243+shakedS10@users.noreply.github.com>
2025-06-21 19:44:15 +0300

    Delete py311_env directory

--- py311_env/Lib/site-packages/clarabel/__init__.py
+++
@@ -1,5 +0,0 @@
-from .clarabel import *
-
-__doc__ = clarabel.__doc__
-if hasattr(clarabel, "__all__"):
-    __all__ = clarabel.__all__
--- py311_env/Lib/site-packages/osqp/tests/utils.py
+++
@@ -1,8 +0,0 @@
-import os.path
-import numpy as np
-
-
-def load_high_accuracy(test_name):
-    npz = os.path.join(os.path.dirname(__file__), 'solutions', f'{test_name}.npz')
-    npzfile = np.load(npz)
-    return npzfile['x_val'], npzfile['y_val'], npzfile['obj']
